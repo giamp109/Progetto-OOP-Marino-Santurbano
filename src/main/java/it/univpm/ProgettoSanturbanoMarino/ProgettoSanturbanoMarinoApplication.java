68982225cb5522ab7615f276492bb75220d0a473
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-02 17:46:43 +0100

    Loading initial files created with Spring Initializr

---
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/ProgettoSanturbanoMarinoApplication.java
@@ -0,0 +1,18 @@
+package it.univpm.ProgettoSanturbanoMarino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class ProgettoSanturbanoMarinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSanturbanoMarinoApplication.class, args);
+	}
+	
+
+
+}
---
+++ src/test/java/it/univpm/ProgettoSanturbanoMarino/ProgettoSanturbanoMarinoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.ProgettoSanturbanoMarino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSanturbanoMarinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
