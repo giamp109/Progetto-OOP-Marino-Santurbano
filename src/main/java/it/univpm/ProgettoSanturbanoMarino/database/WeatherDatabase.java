31177a6ce1cda061c6b861fb4f005d3688eff032
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-02 17:58:22 +0100

    WeatherDatabase import from Eclipse.
    WeatherDatabase contains the link via URL with the OpenWeather API, it returns as a result the JSON file with the weather information regarding the city

---
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/database/WeatherDatabase.java
@@ -0,0 +1,55 @@
+package it.univpm.ProgettoSanturbanoMarino.database;
+import  it.univpm.ProgettoSanturbanoMarino.model.City;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+
+public class WeatherDatabase {
+		   
+			
+			
+			public static JSONObject getForecast(String City) {
+				 String data = "";
+				 String line = "";
+				 String APIkey="839a51d0900812c291d2ba48954f3052";
+				 String url = "https://api.openweathermap.org/data/2.5/forecast?q=";
+				JSONObject Forecast = null;
+				try {
+					URLConnection OpenConnection = new URL(url+City+"&appid="+APIkey).openConnection();	
+					InputStream in = OpenConnection.getInputStream();	
+					try {
+						InputStreamReader inR = new InputStreamReader(in);
+						BufferedReader BUF= new BufferedReader(inR);
+						
+						while ((line = BUF.readLine()) != null) {
+							data += line;
+						}
+					}finally {
+					in.close();
+				}
+					Forecast=(JSONObject) JSONValue.parseWithException(data);
+	}catch (IOException  e) {
+		e.printStackTrace();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+		/*System.out.println(Forecast);*/
+				return Forecast;		
+			}
+
+
+	
+			
+
+           
+}
+
+
