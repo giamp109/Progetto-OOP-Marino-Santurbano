71af3085de5d9a2b57eea7165fbcd07821246f63
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-02 18:03:49 +0100

    importing from Eclipse the util folder
    This folder contains the files related to stats and filters

---
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/util/WeatherStats.java
@@ -0,0 +1,14 @@
+package it.univpm.ProgettoSanturbanoMarino.util;
+
+import it.univpm.ProgettoSanturbanoMarino.model.City;
+import it.univpm.ProgettoSanturbanoMarino.service.JSONParse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+
+public class WeatherStats {
+
+	int hu_max, hu_min; 
+	
+}
