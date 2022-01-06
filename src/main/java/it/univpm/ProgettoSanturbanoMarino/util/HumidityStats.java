e9bcf9430469da5c6bfb6d6eee5abe7ed0b0ebff
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-06 11:51:21 +0100

    Update HumidityStats.java
    Addition of the HumidityMaxMin method which calculates the maximum and minimum value among all the filtered humidity values

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/util/HumidityStats.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/util/HumidityStats.java
@@ -1,1 +1,33 @@
+package it.univpm.ProgettoSanturbanoMarino.util;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
+
+import it.univpm.ProgettoSanturbanoMarino.model.*;
+
+public class HumidityStats {
+
+	public static String HumidityMaxMin(City currentcity) {
+		int humiditymax=currentcity.getForecastlist().get(0).getHumidity(), humiditymin=currentcity.getForecastlist().get(0).getHumidity();
+		for(int i=1;i<currentcity.getForecastlist().size();i++) {
+			if (humiditymax<currentcity.getForecastlist().get(i).getHumidity())humiditymax=currentcity.getForecastlist().get(i).getHumidity();
+			if (humiditymin>currentcity.getForecastlist().get(i).getHumidity())humiditymin=currentcity.getForecastlist().get(i).getHumidity();
+		}
+		
+	    String result="maximum humidity: "+humiditymax+" minimum humidity : "+humiditymin;		
+		return result;
+		}
+	
+	public static String HumidityAverage(City currentcity) {
+		double average,sumhumidity=0;
+		for (int i=0;i<currentcity.getForecastlist().size();i++) sumhumidity+= currentcity.getForecastlist().get(i).getHumidity();
+		average=sumhumidity/currentcity.getForecastlist().size();
+		
+		String result=" Humidity Average: "+average;
+		return result;
+		
+	}
+	
