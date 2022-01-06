3779f7336a1959bddeee9a49af2cb4378bb401d7
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-06 12:00:20 +0100

    Update JSONFileParser.java
    Implementation of the FileParse method that allows to obtain a JSON file starting from a city object.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONFileParser.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONFileParser.java
@@ -4,27 +4,61 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import it.univpm.ProgettoSanturbanoMarino.model.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class JSONFileParser {
 	   
 public static City FileParse(String cityname) throws FileNotFoundException, IOException, ParseException {
+	
+	String id,name,country,main,description;
+	long prov;
+	LocalDate date;
+	LocalTime time;
+	double temp,tempmin,tempmax,feelslike;
+	int humidity;
+	Time temporary;
+	
+   String path = System.getProperty("user.dir")+"\\"+cityname+".txt";
+   JSONParser parser = new JSONParser();
+   Object obj = parser.parse(new FileReader(path));
+   
+   JSONObject object = (JSONObject) obj;   
+   JSONArray objectarray= (JSONArray)object.get("Weather");   
+
+	
+	id=(String)(object.get("id"));
+	name=(String)(object.get("name"));
+    country=(String)(object.get("Country"));
+    
+    City parsedcity=new City(id,name,country);
+    
+   for (int i=0; i<objectarray.size();i++) {
+    	JSONObject weatherinfo=(JSONObject) objectarray.get(i);
+    	
+    	humidity = Integer.parseInt(weatherinfo.get("humidity").toString());
+    	
+    	temp=Double.parseDouble(weatherinfo.get("temp").toString());
+		tempmin=Double.parseDouble(weatherinfo.get("temp_min").toString());
+		tempmax=Double.parseDouble(weatherinfo.get("temp_max").toString());
+		feelslike=Double.parseDouble(weatherinfo.get("feels_like").toString());
+    	
+		date=LocalDate.parse(weatherinfo.get("date").toString());
+    	time=LocalTime.parse(weatherinfo.get("time").toString());
+    	
+    	main=(String)weatherinfo.get("main");
+		description=(String)weatherinfo.get("description");
+		temporary=new Time(humidity,main,description,temp,tempmin,tempmax,feelslike,date,time);
+		parsedcity.getForecastlist().add(temporary);
+		
+    }
 	   
-	try {
-	   String path = System.getProperty("user.dir")+"\\"+cityname+".txt";
-	   JSONParser parser = new JSONParser();
-	   Object obj = parser.parse(new FileReader(path));
-	   JSONObject object = (JSONObject) obj;   
-	   
-	}catch(FileNotFoundException e) {
-		   e.printStackTrace();
-	   }
-	   
-	   
-	return null;
+	return parsedcity;
 	}
 }
