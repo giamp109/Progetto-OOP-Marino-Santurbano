3e07845b42050f5fa8f3c40c9f7320d4e1b623c1
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-09 18:43:07 +0100

    Update WeatherDatabaseCurrent.java

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/database/WeatherDatabaseCurrent.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/database/WeatherDatabaseCurrent.java
@@ -1,1 +1,49 @@
+package it.univpm.ProgettoSanturbanoMarino.database;
+
+import it.univpm.ProgettoSanturbanoMarino.exceptions.CityNotFoundException;
+import  it.univpm.ProgettoSanturbanoMarino.model.City;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+
+
+public class WeatherDatabaseCurrent {
+	
+        public static JSONObject getForecastCurrent(String City) throws CityNotFoundException {
+		 String data = "";
+		 String line = "";
+		 String APIkey="839a51d0900812c291d2ba48954f3052";
+		 String url = "https://api.openweathermap.org/data/2.5/weather?q=";
+		JSONObject Forecast = null;
+		try {
+			URLConnection OpenConnection = new URL(url+City+"&units=metric&appid="+APIkey).openConnection();	
+			InputStream in = OpenConnection.getInputStream();	
+			try {
+				InputStreamReader inR = new InputStreamReader(in);
+				BufferedReader BUF= new BufferedReader(inR);
+				
+				while ((line = BUF.readLine()) != null) {
+					data += line;
+				}
+			}finally {
+			in.close();
+		}
+			Forecast=(JSONObject) JSONValue.parseWithException(data);
+       }catch (IOException  e) {
+    	   e.printStackTrace();
+       } catch (Exception e) {
+    	   e.printStackTrace();
+       }
+		if (Forecast == null) throw new CityNotFoundException("City Not Found");
+		return Forecast;		
+	}
+}
 
