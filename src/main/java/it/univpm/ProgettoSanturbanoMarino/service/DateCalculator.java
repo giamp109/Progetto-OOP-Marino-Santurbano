fed5ba2ec3ff24aecc3f55bf272ac4e8593539a6
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-09 19:07:41 +0100

    Update DateCalculator.java

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculator.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculator.java
@@ -1,1 +1,16 @@
+package it.univpm.ProgettoSanturbanoMarino.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class DateCalculator {
+
+	public static LocalDateTime datecalculator(long epoch,int timezone) {
+		LocalDateTime Date = LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC.ofTotalSeconds(timezone));
+		return Date;
+		
+	}
+}
 
