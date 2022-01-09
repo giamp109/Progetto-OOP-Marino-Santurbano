b75dc6e61e615b9622b8922d8b357b17439c52f5
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-09 18:45:34 +0100

    Update CityNotFoundException.java

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
@@ -1,1 +1,16 @@
+package it.univpm.ProgettoSanturbanoMarino.exceptions;
 
+public class DateNotFoundException extends Exception{
+	
+	String errormex;
+	
+	public DateNotFoundException (String errormex) {
+		super();
+		this.errormex=errormex;
+	}
+	
+	public String getExceptionMessage() {
+		return errormex;
+	}
+
+}
