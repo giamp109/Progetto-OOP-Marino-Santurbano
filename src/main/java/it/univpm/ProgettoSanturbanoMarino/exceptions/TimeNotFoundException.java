9c293589d7a1d1fe3c0c475357b13cfd740c1ce4
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 11:56:42 +0100

    Update TimeNotFoundException.java
    Indented and commented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/TimeNotFoundException.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/TimeNotFoundException.java
@@ -1,13 +1,30 @@
 package it.univpm.ProgettoSanturbanoMarino.exceptions;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+* La classe TimeeNotFoundException contiene il metodo che segnala l'eccezione in caso di orario non trovato.
+* 
+*/
 public class TimeNotFoundException extends Exception {
 	String errormex;
-	
+
+	/**
+	*
+	* Costruttore della classe TimeNotFoundException.
+	*
+	*/
 	public TimeNotFoundException (String errormex) {
 		super();
 		this.errormex= errormex;
 	}
 	
+	/**
+	*
+	* Il metodo getExceptionMessage restituisce un messaggio di errore in caso di data non trovata.
+	*
+	*/
 	public String getExceptionMessage() {
 		return errormex;
 	}
