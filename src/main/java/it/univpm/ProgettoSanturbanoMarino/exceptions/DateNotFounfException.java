368b4595d14d29e5d4b5ffeb1fea5c610dafe2fb
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 11:55:15 +0100

    Update DateNotFounfException.java
    Indented and commented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/DateNotFounfException.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/DateNotFounfException.java
@@ -1,14 +1,31 @@
 package it.univpm.ProgettoSanturbanoMarino.exceptions;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+* La classe DateNotFoundException contiene il metodo che segnala l'eccezione in caso di data non trovata.
+* 
+*/
 public class DateNotFoundException extends Exception{
 	
 	String errormex;
 	
+	/**
+	*
+	* Costruttore della classe DateNotFoundException.
+	*
+	*/
 	public DateNotFoundException (String errormex) {
 		super();
 		this.errormex=errormex;
 	}
 	
+	/**
+	*
+	* Il metodo getExceptionMessage restituisce un messgagio di errore in caso di data non trovata.
+	*
+	*/
 	public String getExceptionMessage() {
 		return errormex;
 	}
