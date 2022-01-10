c4c8e67dc6b5a4277172f28f8a3f1a2b21325f20
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 16:32:18 +0100

    Update ErrorForecastCurrent.java
    Indented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/util/ErrorForecastCurrent.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/util/ErrorForecastCurrent.java
@@ -32,26 +32,20 @@ public class ErrorForecastCurrent {
 		LocalDate supportdate= LocalDate.parse(date);
 		LocalTime supportime= LocalTime.parse(time);
 
-		for (int i=0;i<forecastcity.getForecastlist().size();i++) 
-			for (int j=0;j<currentcity.getForecastlist().size();j++) {
-				
-				if(forecastcity.getForecastlist().get(i).getDate().equals(supportdate) && currentcity.getForecastlist().get(j).getDate().equals(supportdate)  )  
-				   if( forecastcity.getForecastlist().get(i).getTime().getHour()== supportime.getHour() && currentcity.getForecastlist().get(j).getTime().getHour() == supportime.getHour())
-				   {
-					    double errortemp=forecastcity.getForecastlist().get(i).getTemperature()-currentcity.getForecastlist().get(j).getTemperature();
-						double errortempmax=forecastcity.getForecastlist().get(i).getTemperatureMax()-currentcity.getForecastlist().get(j).getTemperatureMax();
-						double errortempmin=forecastcity.getForecastlist().get(i).getTemperatureMin()-currentcity.getForecastlist().get(j).getTemperatureMin();
-						double errorfeelslike=forecastcity.getForecastlist().get(i).getFeelsLike()-currentcity.getForecastlist().get(j).getFeelsLike();
-						int errorhumidity=forecastcity.getForecastlist().get(i).getHumidity()-currentcity.getForecastlist().get(j).getHumidity();  					   
-						return "error between forecast and current temperature: "+Math.round(errortemp*100.0)/100.0+"\nerror between forecast and current maximum temperature: "+Math.round(errortempmax*100.0)/100.0+"\nerror between forecast and current minimum temperature :"+Math.round(errortempmin*100.0)/100.0+"\nerror between forecast and current felt like temperature: "+Math.round(errorfeelslike*100.0)/100.0+"\nerror between forecast and current humidity: "+Math.round(errorhumidity*100.0)/100.0 ;
-				   }
-					
-					
-			
+		for (int i=0; i<forecastcity.getForecastlist().size(); i++) 
+			for (int j=0; j<currentcity.getForecastlist().size(); j++) {
+				if(forecastcity.getForecastlist().get(i).getDate().equals(supportdate) && currentcity.getForecastlist().get(j).getDate().equals(supportdate))  
+					if( forecastcity.getForecastlist().get(i).getTime().getHour()== supportime.getHour() && currentcity.getForecastlist().get(j).getTime().getHour() == supportime.getHour())
+					{
+						double errortemp = forecastcity.getForecastlist().get(i).getTemperature() - currentcity.getForecastlist().get(j).getTemperature();
+						double errortempmax = forecastcity.getForecastlist().get(i).getTemperatureMax() - currentcity.getForecastlist().get(j).getTemperatureMax();
+						double errortempmin = forecastcity.getForecastlist().get(i).getTemperatureMin() - currentcity.getForecastlist().get(j).getTemperatureMin();
+						double errorfeelslike = forecastcity.getForecastlist().get(i).getFeelsLike() - currentcity.getForecastlist().get(j).getFeelsLike();
+						int errorhumidity = forecastcity.getForecastlist().get(i).getHumidity() - currentcity.getForecastlist().get(j).getHumidity();  					   
+						return "error between forecast and current temperature: " + Math.round(errortemp*100.0)/100.0+"\nerror between forecast and current maximum temperature: " + Math.round(errortempmax*100.0)/100.0 + "\nerror between forecast and current minimum temperature :" + Math.round(errortempmin*100.0)/100.0 + "\nerror between forecast and current felt like temperature: " + Math.round(errorfeelslike*100.0)/100.0 + "\nerror between forecast and current humidity: " + Math.round(errorhumidity*100.0)/100.0 ;
+					}
 			}
-
-		return "error between forecast and current temperature: null"+"\nerror between forecast and current maximum temperature: null"+"\nerror between forecast and current minimum temperature : null"+"\nerror between forecast and current felt like temperature: null"+"\nerror between forecast and current humidity: null";
-
+			return "error between forecast and current temperature: null"+"\nerror between forecast and current maximum temperature: null"+"\nerror between forecast and current minimum temperature : null"+"\nerror between forecast and current felt like temperature: null"+"\nerror between forecast and current humidity: null";
 	}
 
 }
