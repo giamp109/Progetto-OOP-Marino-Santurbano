acf7aa2fe956dc9bbc94ada2c23f8e812b20445e
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-10 12:51:25 +0100

    Update DateCalculator.java
    Added comments

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculator.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculator.java
@@ -5,8 +5,23 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-public class DateCalculator {
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe DateCalculator contiene il metodo per convertire l'epoca in LocalDateTime.
+*
+*/
 
+public class DateCalculator {
+	
+	/**
+	* Il metodo 'datecalculator()' converte l'epoca in LocalDateTime sfruttando il timezone.
+	* @param epoch che rappresenta l'epoca
+	* @param timezone che rappresenta lo shift in secondi dall'UTC
+	* @return Date che rappresenta l'epoca espressa in ('yyyy-mm-gg hh:mm:ss')
+	*/
 	public static LocalDateTime datecalculator(long epoch,int timezone) {
 		LocalDateTime Date = LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC.ofTotalSeconds(timezone));
 		return Date;
