f60ff7b501dd9abcb071fb0c53ec8cff7bd78fd4
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-10 11:54:12 +0100

    Update CityNotFoundException.java

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
@@ -1,33 +1,15 @@
 package it.univpm.ProgettoSanturbanoMarino.exceptions;
-/**
-*
-* @author Davide Santurbano
-* @author Giampaolo Marino
-* La classe DateNotFoundException contiene il metodo che segnala l'eccezione in caso di data non trovata.
-* 
-*/
-public class DateNotFoundException extends Exception{
+
+public class CityNotFoundException extends Exception {
 	
 	String errormex;
 	
-	/**
-	*
-	* Costruttore della classe DateNotFoundException.
-	*
-	*/
-	public DateNotFoundException (String errormex) {
+	public CityNotFoundException (String errormex) {
 		super();
-		this.errormex=errormex;
+		this.errormex= errormex;
 	}
 	
-	
-	/**
-	*
-	* Il metodo getExceptionMessage restituisce un messgagio di errore in caso di data non trovata.
-	*
-	*/
 	public String getExceptionMessage() {
 		return errormex;
 	}
-
 }
