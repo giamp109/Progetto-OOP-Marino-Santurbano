0d5611c063d087daf3521751141894b90d607d37
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 12:19:10 +0100

    Update WeatherController.java
    Indented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/controller/WeatherController.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/controller/WeatherController.java
@@ -47,11 +47,11 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/saveJSON")
-	public ResponseEntity<Object> saveJSONCurrent(@RequestParam (value="CityName", defaultValue="Ancona")String CityName) throws ParseException, CityNotFoundException, FileNotFoundException{		
+	public ResponseEntity<Object> saveJSONCurrent(@RequestParam (value = "CityName", defaultValue = "Ancona") String CityName) throws ParseException, CityNotFoundException, FileNotFoundException {		
 		try {
-			String path="JSONfile API forecast 5days/3hours saved in: "+JSONFile.saveJSON(CityName)+"\nJSONfile API current saved in: "+JSONFileCurrent.saveJSONCurrent(CityName);
+			String path = "JSONfile API forecast 5days/3hours saved in: " + JSONFile.saveJSON(CityName) + "\nJSONfile API current saved in: " + JSONFileCurrent.saveJSONCurrent(CityName);
 			return new ResponseEntity<>(path, HttpStatus.OK);
-		}catch(CityNotFoundException e) {
+		}catch (CityNotFoundException e) {
 			return new ResponseEntity<>(e.getExceptionMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -67,10 +67,10 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/getCurrentWeather")
-	public ResponseEntity<Object> getForecastCurrent(@RequestParam (value="CityName", defaultValue="Ancona")String CityName) throws ParseException, FileNotFoundException, IOException{		
+	public ResponseEntity<Object> getForecastCurrent(@RequestParam (value = "CityName", defaultValue = "Ancona") String CityName) throws ParseException, FileNotFoundException, IOException{		
 		try {
 			return new ResponseEntity<>(JSONFileCurrentParser.FileCurrentParse(CityName), HttpStatus.OK);
-		}catch(FileNotFoundException e) {
+		}catch (FileNotFoundException e) {
 			return new ResponseEntity<>("Invalid City Name", HttpStatus.OK);
 		}
 	}
@@ -86,7 +86,7 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/getForecastWeather")
-	public ResponseEntity<Object> getForecast(@RequestParam (value="CityName", defaultValue="Ancona")String CityName) throws ParseException, IOException{		
+	public ResponseEntity<Object> getForecast(@RequestParam (value = "CityName", defaultValue = "Ancona") String CityName) throws ParseException, IOException {		
 		try {
 			return new ResponseEntity<>(JSONFileParser.FileParse(CityName), HttpStatus.OK);
 		}catch (FileNotFoundException e) {
@@ -106,10 +106,10 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/filterTime")
-	public ResponseEntity<Object> getfilterTime(@RequestParam(value="CityName", defaultValue="Ancona")String CityName, @RequestParam(value="Time", defaultValue="12:00")String Time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException   {
+	public ResponseEntity<Object> getfilterTime(@RequestParam(value = "CityName", defaultValue = "Ancona") String CityName, @RequestParam(value = "Time", defaultValue = "12:00") String Time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException   {
 		try {
 			return new ResponseEntity<>(Filter.timeslot(CityName, Time), HttpStatus.OK);
-		}catch(TimeNotFoundException e) {
+		}catch (TimeNotFoundException e) {
 			return new ResponseEntity<>(e.getExceptionMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -126,10 +126,10 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/filterOneDay")
-	public ResponseEntity<Object> getfilterOneDay(@RequestParam(value="CityName", defaultValue="Ancona")String CityName, @RequestParam(value="Date", defaultValue="2022-01-06")String Date) throws ParseException, FileNotFoundException, IOException, DateNotFoundException   {
+	public ResponseEntity<Object> getfilterOneDay(@RequestParam(value = "CityName", defaultValue = "Ancona") String CityName, @RequestParam(value = "Date", defaultValue = "2022-01-06") String Date) throws ParseException, FileNotFoundException, IOException, DateNotFoundException   {
 		try {
 			return new ResponseEntity<>(Filter.onedayslot(CityName, Date), HttpStatus.OK);
-		}catch(DateNotFoundException e ) {
+		}catch (DateNotFoundException e ) {
 			return new ResponseEntity<>(e.getExceptionMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -145,7 +145,7 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/filterFiveDays")
-	public ResponseEntity<Object> getfilterFiveDays(@RequestParam(value="CityName", defaultValue="Ancona")String CityName) throws ParseException, FileNotFoundException, IOException   {
+	public ResponseEntity<Object> getfilterFiveDays(@RequestParam(value = "CityName", defaultValue = "Ancona") String CityName) throws ParseException, FileNotFoundException, IOException   {
 		return new ResponseEntity<>(Filter.fivedaysslot(CityName), HttpStatus.OK);
 	}
 		
@@ -161,11 +161,11 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/HumidityStatsTime")
-	public ResponseEntity<Object> HumidityStatsTime(@RequestParam(value="CityName", defaultValue="Ancona")String CityName, @RequestParam(value="Time", defaultValue="12:00")String Time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException   {
+	public ResponseEntity<Object> HumidityStatsTime(@RequestParam(value = "CityName", defaultValue = "Ancona") String CityName, @RequestParam(value = "Time", defaultValue = "12:00") String Time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException   {
 		try {
-			String stats=HumidityStats.HumidityMaxMin(Filter.timeslot(CityName, Time))+HumidityStats.HumidityAverage(Filter.timeslot(CityName, Time))+HumidityStats.HumidityVariance(Filter.timeslot(CityName, Time));
+			String stats = HumidityStats.HumidityMaxMin(Filter.timeslot(CityName, Time)) + HumidityStats.HumidityAverage(Filter.timeslot(CityName, Time)) + HumidityStats.HumidityVariance(Filter.timeslot(CityName, Time));
 			return new ResponseEntity<>(stats, HttpStatus.OK);
-		}catch(TimeNotFoundException e) {
+		}catch (TimeNotFoundException e) {
 			return new ResponseEntity<>(e.getExceptionMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -185,9 +185,9 @@ public class WeatherController {
 	@GetMapping("/HumidityStatsOneDay")
 	public ResponseEntity<Object> HumidityStatsOneDay(@RequestParam(value="CityName", defaultValue="Ancona")String CityName, @RequestParam(value="Date", defaultValue="2022-01-05")String Date) throws ParseException, FileNotFoundException, IOException, DateNotFoundException  {
 		try {
-			String stats=HumidityStats.HumidityMaxMin(Filter.onedayslot(CityName, Date))+HumidityStats.HumidityAverage(Filter.onedayslot(CityName, Date))+HumidityStats.HumidityVariance(Filter.onedayslot(CityName, Date));
+			String stats = HumidityStats.HumidityMaxMin(Filter.onedayslot(CityName, Date)) + HumidityStats.HumidityAverage(Filter.onedayslot(CityName, Date)) + HumidityStats.HumidityVariance(Filter.onedayslot(CityName, Date));
 			return new ResponseEntity<>(stats, HttpStatus.OK);
-		}catch(DateNotFoundException e) {
+		}catch (DateNotFoundException e) {
 			return new ResponseEntity<>(e.getExceptionMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -203,8 +203,8 @@ public class WeatherController {
 	*/
 	
 	@GetMapping("/HumidityStatsFiveDays")
-	public ResponseEntity<Object> HumidityStatsFiveDays(@RequestParam(value="CityName", defaultValue="Ancona")String CityName) throws ParseException, FileNotFoundException, IOException   {
-		String stats=HumidityStats.HumidityMaxMin(Filter.fivedaysslot(CityName))+HumidityStats.HumidityAverage(Filter.fivedaysslot(CityName))+HumidityStats.HumidityVariance(Filter.fivedaysslot(CityName));
+	public ResponseEntity<Object> HumidityStatsFiveDays(@RequestParam(value = "CityName", defaultValue = "Ancona") String CityName) throws ParseException, FileNotFoundException, IOException   {
+		String stats = HumidityStats.HumidityMaxMin(Filter.fivedaysslot(CityName)) + HumidityStats.HumidityAverage(Filter.fivedaysslot(CityName)) + HumidityStats.HumidityVariance(Filter.fivedaysslot(CityName));
 		return new ResponseEntity<>(stats, HttpStatus.OK);
 	}
 	
@@ -222,10 +222,10 @@ public class WeatherController {
 	*/
 		
 	@GetMapping("/ErrorCurrentForecast")
-	public ResponseEntity<Object> getErrorForecastCurrent(@RequestParam (value="CityName", defaultValue="Ancona")String CityName, @RequestParam(value="Date", defaultValue="2022-01-07")String Date, @RequestParam(value="Time", defaultValue="18:00")String Time) throws ParseException, FileNotFoundException, IOException{			
+	public ResponseEntity<Object> getErrorForecastCurrent(@RequestParam (value = "CityName", defaultValue = "Ancona") String CityName, @RequestParam(value = "Date", defaultValue = "2022-01-07") String Date, @RequestParam(value = "Time", defaultValue = "18:00") String Time) throws ParseException, FileNotFoundException, IOException{			
 		try {
 			return new ResponseEntity<>(ErrorForecastCurrent.ErrorCalculator(JSONFileCurrentParser.FileCurrentParse(CityName), JSONFileParser.FileParse(CityName), Date, Time), HttpStatus.OK);	
-		}catch(FileNotFoundException e) {
+		}catch (FileNotFoundException e) {
 			return new ResponseEntity<>("File not found, use the route 'save JSON' to crate the files", HttpStatus.BAD_REQUEST);
 		}
 	}
