352422287223129c38e8643233b6e8d455706e92
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 16:25:47 +0100

    Update Time.java
    Commented code

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/model/Time.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/model/Time.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 *
 * @author Davide Santurbano
 * @author Giampaolo Marino
-* La classe Time contiene il metodo che segnala l'eccezione in caso di data non trovata.
+* La classe Time definisce l'oggetto 'Time' e le relative variabili .
 * 
 */
 public class Time extends Temperature{
@@ -17,20 +17,53 @@ public class Time extends Temperature{
 	LocalDate date;
 	LocalTime time;
 	
+	/**
+	*
+	* Costruttore della classe Time.
+	*
+	*/
 	public Time(int humidity, String main, String description, double temperature, double temperaturemin, double temperaturemax, double feelslike, LocalDate date, LocalTime time) {
 		super(humidity, main, description, temperature, temperaturemin, temperaturemax, feelslike);
 		this.date = date;
 		this.time = time;
 	}
+	
+	/**
+	*
+	* Il metodo 'getDate()' restituisce il valore della data
+	* @return date
+	*
+	*/
 	public LocalDate getDate() {
 		return date;
 	}
+	
+	/**
+	*
+	* Il metodo 'setDate()' setta il valore della data
+	* @param data
+	* 
+	*/
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+	
+	/**
+	*
+	* Il metodo 'getTime()' restituisce il valore dell'ora
+	* @return time
+	*
+	*/
 	public LocalTime getTime() {
 		return time;
 	}
+	
+	/**
+	*
+	* Il metodo 'setTime()' setta il valore dell'ora
+	* @param time
+	* 
+	*/
 	public void setTime(LocalTime time) {
 		this.time = time;
 	}
