6223b8e138b497ebd8bafa9cb58ad88f8da9b619
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-10 16:33:56 +0100

    Update HumidityStats.java
    Added comments.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/util/HumidityStats.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/util/HumidityStats.java
@@ -4,13 +4,27 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-
-
 import it.univpm.ProgettoSanturbanoMarino.model.*;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe HumidityStats mette a disposizione i metodi per il calcolo delle statistiche sull'umidità.
+* 
+*/
 public class HumidityStats {
 
-static String  result;
+static String result;
+	
+	/**
+	*
+	* il metodo 'HumidityMaxMin()' permette di trovare l'umidità massima e l'umidità minima tra tutti i valori analizzati.  
+	* 
+	* @param City currentcity, oggetto di tipo City di cui viene analizzato la ForecastList in modo da effettuaree le statistiche sull'umidità.
+	* @return String result contenente il valore massimo e il valore minimo dell'umidità.
+	*/
 	public static String HumidityMaxMin(City currentcity) {
 		int humiditymax=currentcity.getForecastlist().get(0).getHumidity(), humiditymin=currentcity.getForecastlist().get(0).getHumidity();
 		for(int i=1;i<currentcity.getForecastlist().size();i++) {
@@ -22,6 +36,13 @@ static String  result;
 		return result;
 		}
 	
+	/**
+	*
+	* il metodo 'HumidityAverage()' permette di calcolare la media dell'umidità tra tutti i valori analizzati.  
+	* 
+	* @param City currentcity, oggetto di tipo City di cui viene analizzato la ForecastList in modo da effettuaree le statistiche sull'umidità.
+	* @return String result, contenente il valore della media dell'umidità.
+	*/
 	public static String HumidityAverage(City currentcity) {
 		double average,sumhumidity=0;
 		for (int i=0;i<currentcity.getForecastlist().size();i++) sumhumidity+= currentcity.getForecastlist().get(i).getHumidity();
@@ -32,6 +53,13 @@ static String  result;
 		
 	}
 	
+	/**
+	*
+	* il metodo 'HumidityVariance()' permette di calcolare la varianza dell'umidità tra tutti i valori analizzati.  
+	* 
+	* @param City currentcity, oggetto di tipo City di cui viene analizzato la ForecastList in modo da effettuaree le statistiche sull'umidità.
+	* @return String result, contenente il valore della varianza dell'umidità.
+	*/
 	public static String HumidityVariance(City currentcity) {
 		double average,sumhumidity=0, variance=0, sumvariance=0;
 		for (int i=0;i<currentcity.getForecastlist().size();i++) sumhumidity+= currentcity.getForecastlist().get(i).getHumidity();
