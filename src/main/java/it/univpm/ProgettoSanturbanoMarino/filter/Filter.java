2fe4daed087749d3a55a4fdec9f37e9112db1e3b
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 12:01:11 +0100

    Update Filter.java
    Indented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/filter/Filter.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/filter/Filter.java
@@ -19,50 +19,45 @@ import java.util.Iterator;
 
 public class Filter {
 	
-	    
-	public static City timeslot(String cityname,String time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException{
-           
-	   boolean find=false;
-	       
-       City city=JSONFileParser.FileParse(cityname);
-       City filteredcity = new City(city.getId(),city.getCityname(),city.getCitycountry());
-       LocalTime timeparsed= LocalTime.parse(time);
-		  
-	       for (int i=0;i<city.getForecastlist().size();i++) {			   
-			
-	    	if(city.getForecastlist().get(i).getTime().equals(timeparsed)) {
-	    		filteredcity.getForecastlist().add(city.getForecastlist().get(i));	
-	    		find=true;
-	    	}
-	       }
-	       
-	       if(!find) throw new TimeNotFoundException("Time Not Found");
-		   return filteredcity;	   
-	   }
-	     
-	     
-	public static City onedayslot(String cityname,String date) throws ParseException, FileNotFoundException, IOException, DateNotFoundException{
-	          
-		      boolean find=false;
-		      
-		      City city=JSONFileParser.FileParse(cityname);
-		      City filteredcity = new City(city.getId(),city.getCityname(),city.getCitycountry());
-		      LocalDate dateparsed= LocalDate.parse(date);
-			  
-		       for (int i=0;i<city.getForecastlist().size();i++) {			   
-				
-		    	if(city.getForecastlist().get(i).getDate().equals(dateparsed)) 
-		    	{
-		    		filteredcity.getForecastlist().add(city.getForecastlist().get(i));		
-		    		find=true;
-		    	}
-		    	}
-		       if(!find) throw new DateNotFoundException("Date Not Found");
-			   return filteredcity;	
-	      }
-	     
+	public static City timeslot(String cityname, String time) throws ParseException, FileNotFoundException, IOException, TimeNotFoundException {
+
+		boolean find = false;
+
+		City city = JSONFileParser.FileParse(cityname);
+		City filteredcity = new City(city.getId(), city.getCityname(), city.getCitycountry());
+		LocalTime timeparsed = LocalTime.parse(time);
+
+		for (int i=0; i<city.getForecastlist().size(); i++) {			   
+			if(city.getForecastlist().get(i).getTime().equals(timeparsed)) {
+				filteredcity.getForecastlist().add(city.getForecastlist().get(i));	
+				find = true;
+			}
+		}
+		if(!find) throw new TimeNotFoundException("Time Not Found");
+		return filteredcity;	   
+	}
+
+
+	public static City onedayslot(String cityname, String date) throws ParseException, FileNotFoundException, IOException, DateNotFoundException {
+
+		boolean find=false;
+
+		City city = JSONFileParser.FileParse(cityname);
+		City filteredcity = new City(city.getId(), city.getCityname(),city.getCitycountry());
+		LocalDate dateparsed = LocalDate.parse(date);
+
+		for (int i=0; i<city.getForecastlist().size(); i++) {			   
+
+			if(city.getForecastlist().get(i).getDate().equals(dateparsed)) {
+				filteredcity.getForecastlist().add(city.getForecastlist().get(i));		
+				find = true;
+			}
+		}
+		if(!find) throw new DateNotFoundException("Date Not Found");
+		return filteredcity;	
+	}
+
 	public static City fivedaysslot(String cityname) throws ParseException, FileNotFoundException, IOException{
-			return JSONFileParser.FileParse(cityname);
-	    	  
-	      }
+		return JSONFileParser.FileParse(cityname);
+	}
 }
