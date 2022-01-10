deb365abd5225661f6d848a5bb881f0d373acad2
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 12:23:00 +0100

    Update CityNotFoundException.java
    Indented and commented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/exceptions/CityNotFoundException.java
@@ -1,14 +1,31 @@
 package it.univpm.ProgettoSanturbanoMarino.exceptions;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+* La classe CityNotFoundException contiene il metodo che segnala l'eccezione in caso di città non trovata.
+* 
+*/
 public class CityNotFoundException extends Exception {
 	
 	String errormex;
 	
+	/**
+	*
+	* Costruttore della classe CityNotFoundException.
+	*
+	*/
 	public CityNotFoundException (String errormex) {
 		super();
-		this.errormex= errormex;
+		this.errormex = errormex;
 	}
 	
+	/**
+	*
+	* Il metodo getExceptionMessage restituisce un messaggio di errore in caso di città non trovata.
+	*
+	*/
 	public String getExceptionMessage() {
 		return errormex;
 	}
