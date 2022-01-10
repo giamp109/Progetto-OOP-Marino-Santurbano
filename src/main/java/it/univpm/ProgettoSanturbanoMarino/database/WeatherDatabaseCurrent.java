39da3c04f20449d7f76d131a5e5a9a673180fc74
giamp109 <95345801+giamp109@users.noreply.github.com>
2022-01-10 11:46:13 +0100

    Update WeatherDatabaseCurrent.java
    Indented code.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/database/WeatherDatabaseCurrent.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/database/WeatherDatabaseCurrent.java
@@ -33,32 +33,32 @@ public class WeatherDatabaseCurrent {
 	* @return JSONObject contenente le previsioni meteo della citta presa in considerazione.
 	*
 	*/
-       
+
 	public static JSONObject getForecastCurrent(String City) throws CityNotFoundException {
-		 String data = "";
-		 String line = "";
-		 String APIkey="839a51d0900812c291d2ba48954f3052";
-		 String url = "https://api.openweathermap.org/data/2.5/weather?q=";
+		String data = "";
+		String line = "";
+		String APIkey="839a51d0900812c291d2ba48954f3052";
+		String url = "https://api.openweathermap.org/data/2.5/weather?q=";
 		JSONObject Forecast = null;
 		try {
 			URLConnection OpenConnection = new URL(url+City+"&units=metric&appid="+APIkey).openConnection();	
 			InputStream in = OpenConnection.getInputStream();	
-			try {
-				InputStreamReader inR = new InputStreamReader(in);
-				BufferedReader BUF= new BufferedReader(inR);
-				
-				while ((line = BUF.readLine()) != null) {
-					data += line;
-				}
-			}finally {
+		try {
+			InputStreamReader inR = new InputStreamReader(in);
+			BufferedReader BUF= new BufferedReader(inR);
+
+			while ((line = BUF.readLine()) != null) {
+				data += line;
+			}
+		}finally {
 			in.close();
 		}
-			Forecast=(JSONObject) JSONValue.parseWithException(data);
-       }catch (IOException  e) {
-    	   e.printStackTrace();
-       } catch (Exception e) {
-    	   e.printStackTrace();
-       }
+		Forecast=(JSONObject) JSONValue.parseWithException(data);
+		}catch (IOException  e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		if (Forecast == null) throw new CityNotFoundException("City Not Found");
 		return Forecast;		
 	}
