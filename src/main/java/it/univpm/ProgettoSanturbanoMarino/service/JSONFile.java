d209ab01e16791f91f273c5640c831dffc2ac23a
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-10 13:13:33 +0100

    Update JSONFile.java
    Added comments.

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONFile.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONFile.java
@@ -14,8 +14,25 @@ import it.univpm.ProgettoSanturbanoMarino.database.WeatherDatabase;
 import it.univpm.ProgettoSanturbanoMarino.exceptions.CityNotFoundException;
 import it.univpm.ProgettoSanturbanoMarino.model.*;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe JSONFile contiene il metodo per creare il JSONObject a partire dall'oggetto City.
+*
+*/
+
 public class JSONFile {
 	
+	/**
+	*
+	* Il metodo saveJSON permette di creare il JSONObject a partire dall'oggeto City ricavato dal metodo 'ParseCity()' della classe 'JSONParse'
+	* e di creare il file (nel caso non esista) "cityname.txt" e scriverci sopra il JSONObject creato.
+	*
+	* @param cityname utilizzata per ricavare l'ogetto city e per denominare il file (txt) dove verrà trascritto il JSONObject.
+	* @return path che indica la destinazione in cui il file viene salvato.
+	*/
 	public static String saveJSON(String cityname) throws ParseException, CityNotFoundException, FileNotFoundException {
 		
 		City currentcity=JSONParse.ParseCity(cityname);
@@ -32,18 +49,19 @@ public class JSONFile {
 		
 		
 		for (int i=0;i<currentcity.getForecastlist().size();i++) {
-          JSONObject tempweather=new JSONObject();
-          tempweather.put("temp",currentcity.getForecastlist().get(i).getTemperature());
-          tempweather.put("temp_max",currentcity.getForecastlist().get(i).getTemperatureMax());
-          tempweather.put("temp_min",currentcity.getForecastlist().get(i).getTemperatureMin());
-          tempweather.put("feels_like",currentcity.getForecastlist().get(i).getFeelsLike());
-          tempweather.put("humidity",currentcity.getForecastlist().get(i).getHumidity());
-          tempweather.put("date",currentcity.getForecastlist().get(i).getDate());
-          tempweather.put("time",currentcity.getForecastlist().get(i).getTime());
-          tempweather.put("main",currentcity.getForecastlist().get(i).getMain());
-          tempweather.put("description",currentcity.getForecastlist().get(i).getDescription());
-          temparray.put(tempweather);
+			  JSONObject tempweather=new JSONObject();
+			  tempweather.put("temp",currentcity.getForecastlist().get(i).getTemperature());
+			  tempweather.put("temp_max",currentcity.getForecastlist().get(i).getTemperatureMax());
+			  tempweather.put("temp_min",currentcity.getForecastlist().get(i).getTemperatureMin());
+			  tempweather.put("feels_like",currentcity.getForecastlist().get(i).getFeelsLike());
+			  tempweather.put("humidity",currentcity.getForecastlist().get(i).getHumidity());
+			  tempweather.put("date",currentcity.getForecastlist().get(i).getDate());
+			  tempweather.put("time",currentcity.getForecastlist().get(i).getTime());
+			  tempweather.put("main",currentcity.getForecastlist().get(i).getMain());
+			  tempweather.put("description",currentcity.getForecastlist().get(i).getDescription());
+			  temparray.put(tempweather);
 		}
+		
 		object.put("Weather",temparray);
 		
 		try{
