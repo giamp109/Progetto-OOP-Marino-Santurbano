4f77414b21c75039aabd73cf0fccfc4854ec6a6b
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-11 12:35:54 +0100

    Update JSONParseCurrent.java

--- src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONParseCurrent.java
+++ src/main/java/it/univpm/ProgettoSanturbanoMarino/service/JSONParseCurrent.java
@@ -13,58 +13,74 @@ import it.univpm.ProgettoSanturbanoMarino.model.*;
 import it.univpm.ProgettoSanturbanoMarino.database.WeatherDatabaseCurrent;
 import it.univpm.ProgettoSanturbanoMarino.exceptions.CityNotFoundException;
 
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe JSONParseCurrent mette a disposizione il metodo per effettuare il parsing del JSONObject preso tramite la chiamata all'API Current Weather Data.
+* 
+*/
 public class JSONParseCurrent {
 
-public static City ParseCity(String cityname) throws CityNotFoundException{
+	/**
+	*
+	* Il metodo 'ParseCity()' effettua la chiamata all'API tramite il metodo 'getForecastCurrent()' della classe 'WeatherDatabaseCurrent' e effettua
+	* il parsing delle informazioni creando un oggetto di tipo City.
+	*
+	* @param cityname rappresenta la città di cui si vuole conoscere le previsioni meteo
+	* @return City parsedcity
+	*/
+	public static City ParseCity(String cityname) throws CityNotFoundException{
 
-	Time temporary;
-	
-	long dt;		
-	double temp,tempmin,tempmax,feelslike;
-	int humidity,timezone;
-	String id,name,country,mainweather,description;
-	LocalDateTime datetime;
-	LocalDate date;
-	LocalTime time;
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	
-	JSONObject databasecurrent=WeatherDatabaseCurrent.getForecastCurrent(cityname);
-	JSONObject main=(JSONObject) databasecurrent.get("main");
-	JSONArray weatherarray=(JSONArray) databasecurrent.get("weather");
-	JSONObject weather=(JSONObject) weatherarray.get(0);
-	JSONObject sys=(JSONObject) databasecurrent.get("sys");
-	
-	id=databasecurrent.get("id").toString();
-	name=(String)(databasecurrent.get("name"));
-    country=(String)(sys.get("country"));
-    
-    City parsedcity= new City(id,name,country);
-	
-	humidity = Integer.parseInt(main.get("humidity").toString());
+		Time temporary;
 
-	temp=Double.parseDouble(main.get("temp").toString());
-	tempmin=Double.parseDouble(main.get("temp_min").toString());
-	tempmax=Double.parseDouble(main.get("temp_max").toString());
-	feelslike=Double.parseDouble(main.get("feels_like").toString());
-	
-	mainweather=(String)(weather.get("main"));
-	description=(String)(weather.get("description"));
-	
-	dt=Long.parseLong(databasecurrent.get("dt").toString());
-	timezone=Integer.parseInt(databasecurrent.get("timezone").toString());
-    
-	datetime=DateCalculator.datecalculator(dt, timezone);
-	date=datetime.toLocalDate();
-	time=datetime.toLocalTime();
-	
-	temporary= new Time(humidity,mainweather,description,temp,tempmin,tempmax,feelslike,date,time);
-	parsedcity.getForecastlist().add(temporary);
-	
-	
-	
-	return parsedcity;
-	
-	
+		long dt;		
+		double temp,tempmin,tempmax,feelslike;
+		int humidity,timezone;
+		String id,name,country,mainweather,description;
+		LocalDateTime datetime;
+		LocalDate date;
+		LocalTime time;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+		JSONObject databasecurrent=WeatherDatabaseCurrent.getForecastCurrent(cityname);
+		JSONObject main=(JSONObject) databasecurrent.get("main");
+		JSONArray weatherarray=(JSONArray) databasecurrent.get("weather");
+		JSONObject weather=(JSONObject) weatherarray.get(0);
+		JSONObject sys=(JSONObject) databasecurrent.get("sys");
+
+		id=databasecurrent.get("id").toString();
+		name=(String)(databasecurrent.get("name"));
+		country=(String)(sys.get("country"));
+
+		City parsedcity= new City(id,name,country);
+
+		humidity = Integer.parseInt(main.get("humidity").toString());
+
+		temp=Double.parseDouble(main.get("temp").toString());
+		tempmin=Double.parseDouble(main.get("temp_min").toString());
+		tempmax=Double.parseDouble(main.get("temp_max").toString());
+		feelslike=Double.parseDouble(main.get("feels_like").toString());
+
+		mainweather=(String)(weather.get("main"));
+		description=(String)(weather.get("description"));
+
+		dt=Long.parseLong(databasecurrent.get("dt").toString());
+		timezone=Integer.parseInt(databasecurrent.get("timezone").toString());
+
+		datetime=DateCalculator.datecalculator(dt, timezone);
+		date=datetime.toLocalDate();
+		time=datetime.toLocalTime();
+
+		temporary= new Time(humidity,mainweather,description,temp,tempmin,tempmax,feelslike,date,time);
+		parsedcity.getForecastlist().add(temporary);
+
+
+
+		return parsedcity;
+
+
 	
 }
 
