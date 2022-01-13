134c075feb723df4e76307c8cb8277e91a296964
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-13 15:03:51 +0100

    Added comments

--- src/test/java/it/univpm/ProgettoSanturbanoMarino/controller/WeatherControllerTest.java
+++ src/test/java/it/univpm/ProgettoSanturbanoMarino/controller/WeatherControllerTest.java
@@ -18,58 +18,85 @@ import it.univpm.ProgettoSanturbanoMarino.exceptions.CityNotFoundException;
 import it.univpm.ProgettoSanturbanoMarino.exceptions.DateNotFoundException;
 import it.univpm.ProgettoSanturbanoMarino.exceptions.TimeNotFoundException;
 
-
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe WeatherControllerTest è utilizzata per effettuare i test sui metodi.
+*/
 class WeatherControllerTest {
 
 	private WeatherController controller;
 	
+	/**
+	*
+	* Vengono inizializzate i componenti utili ai fini dei test.
+	* 
+	*/
 	@BeforeEach
 	void setUp() throws Exception {
 		controller = new WeatherController();
 		
 		}
 	
+	/**
+	* Il test 'saveFile()' verifica se i path in cui vengono salvati i due file (cityname.txt e citynamecurrent.txt) è corretto.
+	* 
+	*/
 	@Test
-    @DisplayName("File saved correctly")
-    void saveFile() throws IOException, ParseException, CityNotFoundException {
+    	@DisplayName("File saved correctly")
+    	void saveFile() throws IOException, ParseException, CityNotFoundException {
     	
-        String cityname = "Trento";
-    	String result="JSONfile API forecast 5days/3hours saved in: "+ System.getProperty("user.dir")+"\\"+cityname+".txt" +"\nJSONfile API current saved in: "+ System.getProperty("user.dir")+"\\"+cityname+"current.txt";
-		
-    	ResponseEntity<String> response= new ResponseEntity<>(result,HttpStatus.OK);
-        assertEquals(controller.saveJSON(cityname),response);        
+		String cityname = "Trento";
+		String result="JSONfile API forecast 5days/3hours saved in: "+ System.getProperty("user.dir")+"\\"+cityname+".txt" +"\nJSONfile API current saved in: "+ System.getProperty("user.dir")+"\\"+cityname+"current.txt";
+
+		ResponseEntity<String> response= new ResponseEntity<>(result,HttpStatus.OK);
+		assertEquals(controller.saveJSON(cityname),response);        
     }
 	
+	/**
+	* Il test 'HumidityStatsTimeControl()' verifica la correttezza delle statistiche dell'umidità (fa riferimento al file 'Ancona.txt') 
+	* eseguite per fascia oraria.
+	*/
 	@Test
-    @DisplayName("Correct Humidity Stats Time")
-    void HumidityStatsTimeControl() throws FileNotFoundException, ParseException, IOException, TimeNotFoundException  {
+   	@DisplayName("Correct Humidity Stats Time")
+    	void HumidityStatsTimeControl() throws FileNotFoundException, ParseException, IOException, TimeNotFoundException  {
     	
-        String cityname = "Ancona";
-        String time="12:00";
-  
+		String cityname = "Ancona";
+		String time="12:00";
+
 		ResponseEntity<String> response= new ResponseEntity<>("maximum humidity: 71 minimum humidity : 44 Humidity Average: 59.8 Humidity Variance: 8.84",HttpStatus.OK);
-        assertEquals(controller.HumidityStatsTime(cityname, time),response);        
+		assertEquals(controller.HumidityStatsTime(cityname, time),response);        
     }
 	
+	/**
+	* Il test 'HumidityStatsOneDayControl()' verifica la correttezza delle statistiche dell'umidità (fa riferimento al file 'Ancona.txt') 
+	* eseguite in un determinato giorno.
+	*/
 	@Test
-    @DisplayName("Correct Humidity Stats OneDay")
-    void HumidityStatsOneDayControl() throws FileNotFoundException, ParseException, IOException, DateNotFoundException  {
+    	@DisplayName("Correct Humidity Stats OneDay")
+    	void HumidityStatsOneDayControl() throws FileNotFoundException, ParseException, IOException, DateNotFoundException  { 
     	
-        String cityname = "Ancona";
-        String date="2022-01-13";
-        
+		String cityname = "Ancona";
+		String date="2022-01-13";
+
 		ResponseEntity<String> response= new ResponseEntity<>("maximum humidity: 78 minimum humidity : 59 Humidity Average: 68.75 Humidity Variance: 5.8",HttpStatus.OK);
-        assertEquals(controller.HumidityStatsOneDay(cityname, date),response);        
+		assertEquals(controller.HumidityStatsOneDay(cityname, date),response);        
     }
 	
+	/**
+	* Il test 'HumidityStatsFiveDayControl()' verifica la correttezza delle statistiche dell'umidità (fa riferimento al file 'Ancona.txt') 
+	* eseguite in un periodo di 5 giorni.
+	*/
 	@Test
-    @DisplayName("Correct Humidity Stats Five Days")
-    void HumidityStatsFiveDayControl() throws FileNotFoundException, ParseException, IOException {
+    	@DisplayName("Correct Humidity Stats Five Days")
+    	void HumidityStatsFiveDayControl() throws FileNotFoundException, ParseException, IOException {
     	
-        String cityname = "Ancona";
+		String cityname = "Ancona";
 
 		ResponseEntity<String> response= new ResponseEntity<>("maximum humidity: 89 minimum humidity : 43 Humidity Average: 67.65 Humidity Variance: 10.18",HttpStatus.OK);
-        assertEquals(controller.HumidityStatsFiveDays(cityname),response);        
+		assertEquals(controller.HumidityStatsFiveDays(cityname),response);        
     }
 	
 
