45cf1963c50b6a4925215cfa5fcdfe73702d3170
Ser4ld <95362853+Ser4ld@users.noreply.github.com>
2022-01-13 15:09:39 +0100

    Update DateCalculatorTest.java

--- src/test/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculatorTest.java
+++ src/test/java/it/univpm/ProgettoSanturbanoMarino/service/DateCalculatorTest.java
@@ -14,27 +14,42 @@ import org.junit.jupiter.api.Test;
 
 import it.univpm.ProgettoSanturbanoMarino.exceptions.CityNotFoundException;
 
-
+/**
+*
+* @author Davide Santurbano
+* @author Giampaolo Marino
+*
+* La classe DateCalculatorTest è utilizzata per effettuare i test sui metodi.
+*/
 
 class DateCalculatorTest {
 
 private DateCalculator calculator;
 	
+	/**
+	*
+	* 
+	*/
 	@BeforeEach
 	void setUp() throws Exception {
 		 calculator= new DateCalculator();
 		}
 	
-	@Test
-    @DisplayName("File saved correctly")
-    void saveFile() throws IOException, ParseException, CityNotFoundException {
 		
-        int timezone= 3600;
-        long epoch=1642074049;
+	/**
+	*
+	* il test 'EpochConverter()' permette di testare la giusta esecuzione della conversione da un numero, che indica l'epoca, in una data nel formato (yyyy-mm-dd hh-mm-ss)
+	*/
+	@Test
+    	@DisplayName("conversion performed")
+    	void EpochConverter() throws IOException, ParseException, CityNotFoundException {
+		
+		int timezone= 3600;
+		long epoch=1642074049;
 
-        String datetime="2022-01-13T12:40:49";
+		String datetime="2022-01-13T12:40:49";
 		LocalDateTime datehour = LocalDateTime.parse(datetime);
 
-        assertEquals(DateCalculator.datecalculator(epoch,timezone),datehour);        
+		assertEquals(DateCalculator.datecalculator(epoch,timezone),datehour);        
     }
 }
